34daca49c8883fb31f4948c494e081dc9d60c761
eiiga <ei.tktcwk.ud.f.603@gmail.com>
2023-01-29 17:31:57 +0900

    「文字数チェックツール」のファイル名変更と__doc__を追記

--- python/tool/01_Check79Chars/chk_char_length.py
+++ python/tool/01_Check79Chars/chk_char_length.py
@@ -5,7 +5,23 @@ CHECK_FILE_PATH = './INPUT'
 RESULT_FILE_NAME = './RESULT/check_result.txt'
 CHEK_CHAR_LENGTH = 80
 
-def check_py_chars_length():
+def check_chars_length():
+    '''
+    文字数チェック処理
+    概要：
+        INPUTディレクトリ配下のファイルについて、
+        1行あたりの文字数が規定数以内かチェックし、
+        規定数以上であれば、該当のモジュール名、
+        行数と文字数をRESULTファイルに出力する。
+    引数：
+        なし
+    戻り値：
+        なし
+    INPUT：
+        「./INPUT」ディレクトリ配下（サブディレクトリも含む）のファイル
+    OUTPUT
+        判定NGリストファイル（./RESULT/check_result.txt）
+    '''
     # 出力用ファイルを上書きモードで開く
     with open(RESULT_FILE_NAME, 'w') as out:
         # インプットフォルダ（サブディレクトリ含む）分繰り返し
@@ -40,4 +56,4 @@ def check_py_chars_length():
 
 # メイン処理
 if __name__ == '__main__':
-    check_py_chars_length()
+    check_chars_length()
