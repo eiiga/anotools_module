08a440922854f6e1dcf95b30a0de556bd2a4614b
eiiga <ei.tktcwk.ud.f.603@gmail.com>
2023-01-29 17:34:52 +0900

    「文字数チェックツール」のコード内の改行を追加

--- python/tool/01_Check79Chars/chk_char_length.py
+++ python/tool/01_Check79Chars/chk_char_length.py
@@ -22,34 +22,45 @@ def check_chars_length():
     OUTPUT
         判定NGリストファイル（./RESULT/check_result.txt）
     '''
+    
     # 出力用ファイルを上書きモードで開く
     with open(RESULT_FILE_NAME, 'w') as out:
+        
         # インプットフォルダ（サブディレクトリ含む）分繰り返し
         for current_dir, sub_dir, file_list in os.walk(CHECK_FILE_PATH):
+            
             # ファイルの数だけ繰り返し
             for file_name in file_list:
+                
                 # ファイルのパスをセット
                 tmp_file_path = current_dir + '/' + file_name
+                
                 # ファイル名を出力
                 out.write(tmp_file_path)
                 # 改行
                 out.write('\n')
+                
                 # 読み込みファイルを開く
                 with open(tmp_file_path, 'r') as f:
+                
                     # ファイルの行数分繰り返し
                     for i, line_str in enumerate(f):
+                
                         # 1行当たりの文字数が規定より大きい場合
                         if len(line_str) >= CHEK_CHAR_LENGTH:
+                
                             # 出力用の行数と文字数をセット
                             # ＊「i」は0始まりなのでインクリメント
                             chk_value = str(i+1) \
                                 + '行目：' \
                                 + str(len(line_str)) \
                                 + '文字'
+                            
                             # 判定NGを出力
                             out.write(chk_value)
                             # 改行
                             out.write('\n')
+                
                 # 改行
                 out.write('\n')
 
