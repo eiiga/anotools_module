0cd42edc258d273c2e0d214bc0e05c2afb8d6f05
eiiga <ei.tktcwk.ud.f.603@gmail.com>
2023-11-06 22:38:28 +0900

    【Python】CPU・メモリ使用率出力ツール：完成

--- python/basics/check_cpu_mem/check_cpu_mem.py
+++ python/basics/check_cpu_mem/check_cpu_mem.py
@@ -1,23 +1,43 @@
-import psutil
-import threading
 import datetime
+import psutil
+import schedule
+import time
 
 
 # N分間隔で測定
-INTERVAL_MIN = 1
-# 最計測時間（分）を指定
-MAX_MIN = 60
+INTERVAL_SEC= 60
+# 出力用のヘッダーをセット
+HEADER_INFO = 'time,cpu(%),memory(%)'
 
+# CPUとMemoryの使用率を出力する関数
 def check_cpu_memory():
-    cpu_data = psutil.cpu_percent(percpu=True)
-    print(cpu_data)
-    # print(datetime.datetime.now(), 
-    #       psutil.virtual_memory() )
+    # 現在時刻を取得
+    now_time = datetime.datetime.now()
+    # CPU使用率を取得
+    cpu_data = psutil.cpu_percent(interval=1)
+    # Memoryを取得
+    mem_data = psutil.virtual_memory()
+    
+    # カンマ区切りで「時間、CPU使用率、メモリ使用率」を出力
+    print(now_time, cpu_data, mem_data.percent, sep=',')
 
+
+# メイン処理
 if __name__ == '__main__':
-    check_cpu_memory()
+    # ヘッダーを出力
+    print(HEADER_INFO)
+    
+    # 1分間隔で「CPUとMemoryの使用率を出力する関数」を実行
+    schedule.every(1).minutes.do(check_cpu_memory)
+
+    # 無限ループ
+    while True:
+        # スケジューラを定期実行
+        schedule.run_pending()
+        time.sleep(30)
     
 
 # 参考URL
 # https://pg-chain.com/python-psutil
 # https://qiita.com/john-rocky/items/a0452e6fdb7639b5d044
+# https://di-acc2.com/programming/python/4574/
