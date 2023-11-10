0dbcc037e156ddc7a5d671d48f39526a8e2468e0
eiiga <ei.tktcwk.ud.f.603@gmail.com>
2023-11-11 08:15:44 +0900

    【Python】CPU・メモリ使用率測定ツール：一定間隔起動を「schedule」から「threading.event」に変更

--- python/basics/check_cpu_mem/check_cpu_mem.py
+++ python/basics/check_cpu_mem/check_cpu_mem.py
@@ -1,42 +1,45 @@
 import datetime
+import logging
 import psutil
-import schedule
+import threading
 import time
 
+# 日時：ログレベル：スレッド名：モジュール名：関数名：メッセージを出力する
+LOGGING_FORMAT = '%(asctime)s:' \
+    + '[%(levelname)s]:' \
+    + '[%(threadName)s]:'  \
+    + '[%(module)s]:' \
+    + '[%(funcName)s]:' \
+    + '%(message)s'
 
-# N分間隔で測定
+# ログレベル：デバッグ
+logging.basicConfig(
+    level=logging.DEBUG, format=LOGGING_FORMAT
+)
+
+# N秒間隔で測定
 INTERVAL_SEC= 60
-# 出力用のヘッダーをセット
-HEADER_INFO = 'time,cpu(%),memory(%)'
 
 # CPUとMemoryの使用率を出力する関数
-def check_cpu_memory():
-    # 現在時刻を取得
-    now_time = datetime.datetime.now()
-    # CPU使用率を取得
-    cpu_data = psutil.cpu_percent(interval=1)
-    # Memoryを取得
-    mem_data = psutil.virtual_memory()
-    
-    # カンマ区切りで「時間、CPU使用率、メモリ使用率」を出力
-    print(now_time, cpu_data, mem_data.percent, sep=',')
-
+def check_cpu_memory(event):
+    while not event.wait(INTERVAL_SEC):
+        # CPU使用率を取得
+        cpu_data = psutil.cpu_percent(interval=1)
+        # Memoryを取得
+        mem_data = psutil.virtual_memory()
+        
+        log_msg =  str(cpu_data) + ": " + str(mem_data.percent)
+        
+        # 「CPU使用率、メモリ使用率」を出力
+        logging.debug(log_msg)
 
 # メイン処理
 if __name__ == '__main__':
-    # ヘッダーを出力
-    print(HEADER_INFO)
+     # 1分間隔で「CPUとMemoryの使用率を出力する関数」を実行
+    event = threading.Event()
+    t = threading.Thread(target=check_cpu_memory, args=(event, ))
     
-    # 1分間隔で「CPUとMemoryの使用率を出力する関数」を実行
-    schedule.every(1).minutes.do(check_cpu_memory)
-
-    # 無限ループ
-    while True:
-        # スケジューラを定期実行
-        schedule.run_pending()
-        
-        # 無限ループで実行し続けによる処理負荷がかからないように待機させる
-        time.sleep(30)
+    t.start()
     
 
 # 参考URL
