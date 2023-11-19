b234b6e9260f1017e79deecec07e85550abc6276
eiiga <ei.tktcwk.ud.f.603@gmail.com>
2023-11-20 08:11:40 +0900

    【Python】Tkinter・MySQL：散布図作成ツール：flake8によるソース修正

--- python/tkinter/MakeGraphFromMysql/make_graph_height_weight.py
+++ python/tkinter/MakeGraphFromMysql/make_graph_height_weight.py
@@ -9,7 +9,7 @@ from connect_mysql import ConnectMysql, SelectUserHeightWeght
 def FncInput():
     sql_connect = ConnectMysql()
     connect_info = sql_connect.connector_mysql()
-    
+
     select_user = SelectUserHeightWeght()
     result_data = select_user.select_user_height_weight(connect_info)
     df = pd.DataFrame(result_data)
@@ -25,11 +25,11 @@ if __name__ == '__main__':
     base.title("散布図作成")
     canvas = tk.Canvas(base, width=150, height=50, bd=0, highlightthickness=0)
     canvas.pack()
-    
+
     # 読込ボタン関係の設定
     btn_inputcsv = tk.Button(base, text="散布図作成", command=FncInput)
     btn_inputcsv.place(x=35, y=15)
-    
+
     base.mainloop()
 
 
