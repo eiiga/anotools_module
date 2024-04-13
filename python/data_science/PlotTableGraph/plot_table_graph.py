72c86efdb685b8d510df7f55a2210bbcfe3c2334
eiiga <ei.tktcwk.ud.f.603@gmail.com>
2024-04-13 10:25:07 +0900

    [python] PlotTableGraph: add comments to source.

--- python/data_science/PlotTableGraph/plot_table_graph.py
+++ python/data_science/PlotTableGraph/plot_table_graph.py
@@ -3,24 +3,30 @@ import numpy as np
 import pandas as pd
 
 
-# 先頭をインデックスとしてCSV読み込み
+# 先頭列をインデックスとしてCSV読み込み
 input_data = pd.read_csv('data.csv', index_col=0)
 
-# インデックスを取得：年
-year_rows = list(input_data.index)
-kind_columns = list(input_data.columns)
+# 行列を取得
+year_rows       = list(input_data.index)
+kind_columns    = list(input_data.columns)
+
+# データ部を格納
 datas = input_data.to_numpy()
 
+# カラーマップの設定
 colors = plt.cm.BuPu(np.linspace(0, 0.5, len(year_rows)))
+
+# 行数を取得
 n_rows = len(datas)
 
+# グラフのインデックスと幅を設定
 index = np.arange(len(kind_columns)) + 0.3
 bar_width = 0.4
 
-# Initialize the vertical-offset for the stacked bar chart.
+# 積み上げ棒グラフの垂直オフセットを初期化
 y_offset = np.zeros(len(kind_columns))
 
-# Plot bars and create text labels for the table
+# 棒グラフをプロットし、表のテキストラベルを作成する
 cell_text = []
 for row in range(n_rows):
     plt.bar(index,
@@ -30,22 +36,27 @@ for row in range(n_rows):
         color=colors[row])
     y_offset = y_offset + datas[row]
     cell_text.append([x for x in datas[row]])
-# Reverse colors and text labels to display the last value at the top.
+
+# 色とテキストラベルを反転（降順）
 cell_text.reverse()
 year_rows.reverse()
 colors = colors[::-1]
 
-# Add a table at the bottom of the axes
+# x軸に表を追加
 plt.table(cellText=cell_text,
     rowLabels=year_rows,
     rowColours=colors,
     colLabels=kind_columns,
     loc='bottom')
 
-# Adjust layout to make room for the table:
+# 表のスペースを確保
 plt.subplots_adjust(left=0.2, bottom=0.2)
 
+# ｙ軸ラベルの設定
 plt.ylabel("amount")
+
+# x軸のメモリ設定をなしにする
 plt.xticks([])
 
+# グラフと表の表示
 plt.show()
