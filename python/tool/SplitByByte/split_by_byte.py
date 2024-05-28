d2be6bc5c7da9d8e9cbc21a09f6d06c0994784fb
eiiga <ei.tktcwk.ud.f.603@gmail.com>
2024-05-28 11:48:37 +0900

    [python] SpilitByByte: Create

---
+++ python/tool/SplitByByte/split_by_byte.py
@@ -0,0 +1,71 @@
+import logging
+
+START_VALUE = 'ABC'
+END_VALUE = 'XYZ'
+# 日付|ログレベル|モジュール名|ファンクション名|メッセージ
+LOG_FORMATER = '%(asctime)s|%(levelname)s|%(module)s|%(funcName)s|%(message)s'
+
+# ログレベル
+logging.basicConfig(level=logging.INFO, format=LOG_FORMATER)
+
+# 指定したバイト数で文字列を区切り配列で返す
+def split_by_n_bytes(input_str):
+    # 初期値
+    check_value = ''    # 開始終了文字列判定用
+    result_value = ''   # 実行結果出力用
+    is_data = False     # 開始終了フラグ
+
+    # 文字列を1文字ずつ繰り返す
+    for i in input_str:
+        # 開始終了文字列判定用文字列が3文字より大きい
+        if len(check_value) > 2:
+            # 先頭の1文字を削除し、1文字追加（3文字をキープ）
+            check_value = check_value[1:] + i
+        else:
+            # 1文字追加
+            check_value += i
+
+        # 開始文字列の場合
+        if START_VALUE == check_value:
+            # すでにTrueの場合はNG
+            if is_data:
+                logging.error('開始-終了の間に開始文字列があります：' + input_str)
+                return 1
+            # 開始終了フラグをTrue
+            is_data = True
+            # 実行結果に開始文字列を追加
+            result_value += check_value
+
+            continue
+
+        # 終了文字列の場合
+        elif END_VALUE == check_value:
+            # すでにFalseの場合はNG
+            if not is_data:
+                logging.error('開始の前に終了文字列があります：' + input_str)
+                return 1
+            # 開始終了フラグをFalse
+            is_data = False
+
+            # 終端文字を追加（最後の1文字だけ）
+            result_value += i
+            
+            continue
+
+        if is_data:
+            # 1文字追加
+            result_value += i
+
+    logging.info('実行結果：' + result_value)
+
+    return 0
+
+
+# メイン処理
+if __name__ == '__main__':
+    data1 = 'SRFSABCDEFGHIJKLMNOPQRSTUVWXYZ11'
+    data2 = 'SRFSABCDEFABCGHIJKLMNOPQRSTUVWXYZ11'
+    data3 = 'SRFSABCDEFGHIJKLMNOPQRSTUVWXYZ11XYZ'
+    split_by_n_bytes(data1)
+    split_by_n_bytes(data2)
+    split_by_n_bytes(data3)
