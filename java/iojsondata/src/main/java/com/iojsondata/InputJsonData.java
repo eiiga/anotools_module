fa85e1a5748001042b7c21950b6123aa3b58d4ab
eiiga <ei.tktcwk.ud.f.603@gmail.com>
2024-06-20 21:08:11 +0900

    [java] IoJsonData: add Json data template (input)

--- java/iojsondata/src/main/java/com/iojsondata/InputJsonData.java
+++ java/iojsondata/src/main/java/com/iojsondata/InputJsonData.java
@@ -24,6 +24,16 @@ public class InputJsonData {
 		String name = json_data.get("user_name").textValue();
 		int age = json_data.get("age").intValue();
 		
+		int subdata1_1 = json_data.get("subdata1").get("subdata1-1").intValue();
+		int subdata1_2 = json_data.get("subdata1").get("subdata1-2").intValue();
+		
+		String subdata2_1_1 = json_data.get("subdata2").get(0).get("subdata2-1-1").textValue();
+		String subdata2_1_2 = json_data.get("subdata2").get(0).get("subdata2-1-2").textValue();
+		String subdata2_2_1 = json_data.get("subdata2").get(1).get("subdata2-2-1").textValue();
+		String subdata2_2_2 = json_data.get("subdata2").get(1).get("subdata2-2-2").textValue();
+		String subdata2_3_1 = json_data.get("subdata2").get(2).get("subdata2-3-1").textValue();
+		String subdata2_3_2 = json_data.get("subdata2").get(2).get("subdata2-3-2").textValue();
+		
 		// MAPのインスタンス
 		Map<String, String> user_data_map = new HashMap<>();
 		
@@ -32,10 +42,20 @@ public class InputJsonData {
 		user_data_map.put("user_name", name);
 		user_data_map.put("age", String.valueOf(age));
 		
+		user_data_map.put("subdata1-1", String.valueOf(subdata1_1));
+		user_data_map.put("subdata1-2", String.valueOf(subdata1_2));
+		
+		user_data_map.put("subdata2-1-1", subdata2_1_1);
+		user_data_map.put("subdata2-1-2", subdata2_1_2);
+		user_data_map.put("subdata2-2-1", subdata2_2_1);
+		user_data_map.put("subdata2-2-2", subdata2_2_2);
+		user_data_map.put("subdata2-3-1", subdata2_3_1);
+		user_data_map.put("subdata2-3-2", subdata2_3_2);
+		
 		// MAPの要素数分繰り返し
-		for(String user_data: user_data_map.values()) {
+		for(Map.Entry<String, String> user_data: user_data_map.entrySet()) {
 			// JSONのデータを出力
-			System.out.println(user_data);
+			System.out.println(user_data.getKey() + ": " + user_data.getValue());
 		}
 	}
 
