0b8bbab6734f18ff05a9b912aefcb46424f2a520
eiiga <ei.tktcwk.ud.f.603@gmail.com>
2024-06-22 11:51:38 +0900

    [java] IoJsonData: add comment in UserSubData2.java

--- java/iojsondata/src/main/java/com/iojsondata/UserSubData2.java
+++ java/iojsondata/src/main/java/com/iojsondata/UserSubData2.java
@@ -2,7 +2,7 @@ package com.iojsondata;
 
 public class UserSubData2 {
 
-	public String subdata2_1;
-	public String subdata2_2;
+	public String subdata2_1;	// サブデータ2-1
+	public String subdata2_2;	// サブデータ2-2
 
 }
