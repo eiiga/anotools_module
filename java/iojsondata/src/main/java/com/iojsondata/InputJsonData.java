d1baa2daa7661fa9302edb597f23d2f942673aee
eiiga <ei.tktcwk.ud.f.603@gmail.com>
2024-06-22 11:43:55 +0900

    [Java] IoJsonData: fix json func.

--- java/iojsondata/src/main/java/com/iojsondata/InputJsonData.java
+++ java/iojsondata/src/main/java/com/iojsondata/InputJsonData.java
@@ -27,12 +27,12 @@ public class InputJsonData {
 		int subdata1_1 = json_data.get("subdata1").get("subdata1-1").intValue();
 		int subdata1_2 = json_data.get("subdata1").get("subdata1-2").intValue();
 		
-		String subdata2_1_1 = json_data.get("subdata2").get(0).get("subdata2-1-1").textValue();
-		String subdata2_1_2 = json_data.get("subdata2").get(0).get("subdata2-1-2").textValue();
-		String subdata2_2_1 = json_data.get("subdata2").get(1).get("subdata2-2-1").textValue();
-		String subdata2_2_2 = json_data.get("subdata2").get(1).get("subdata2-2-2").textValue();
-		String subdata2_3_1 = json_data.get("subdata2").get(2).get("subdata2-3-1").textValue();
-		String subdata2_3_2 = json_data.get("subdata2").get(2).get("subdata2-3-2").textValue();
+		String subdata2_1_1 = json_data.get("subdata2").get(0).get("subdata2-1").textValue();
+		String subdata2_2_1 = json_data.get("subdata2").get(0).get("subdata2-2").textValue();
+		String subdata2_1_2 = json_data.get("subdata2").get(1).get("subdata2-1").textValue();
+		String subdata2_2_2 = json_data.get("subdata2").get(1).get("subdata2-2").textValue();
+		String subdata2_1_3 = json_data.get("subdata2").get(2).get("subdata2-1").textValue();
+		String subdata2_2_3 = json_data.get("subdata2").get(2).get("subdata2-2").textValue();
 		
 		// MAPのインスタンス
 		Map<String, String> user_data_map = new HashMap<>();
@@ -45,12 +45,12 @@ public class InputJsonData {
 		user_data_map.put("subdata1-1", String.valueOf(subdata1_1));
 		user_data_map.put("subdata1-2", String.valueOf(subdata1_2));
 		
-		user_data_map.put("subdata2-1-1", subdata2_1_1);
-		user_data_map.put("subdata2-1-2", subdata2_1_2);
-		user_data_map.put("subdata2-2-1", subdata2_2_1);
-		user_data_map.put("subdata2-2-2", subdata2_2_2);
-		user_data_map.put("subdata2-3-1", subdata2_3_1);
-		user_data_map.put("subdata2-3-2", subdata2_3_2);
+		user_data_map.put("subdata2-1_1", subdata2_1_1);
+		user_data_map.put("subdata2-2_1", subdata2_2_1);
+		user_data_map.put("subdata2-1_2", subdata2_1_2);
+		user_data_map.put("subdata2-2_2", subdata2_2_2);
+		user_data_map.put("subdata2-1_3", subdata2_1_3);
+		user_data_map.put("subdata2-2_3", subdata2_2_3);
 		
 		// MAPの要素数分繰り返し
 		for(Map.Entry<String, String> user_data: user_data_map.entrySet()) {
