b8f37874d7a7124930e9246c234b2d4c2ea9c504
eiiga <ei.tktcwk.ud.f.603@gmail.com>
2024-08-24 15:09:22 +0900

    [java] AmountTaxMerge: Created SQL data files(insert).

---
+++ java/amounttaxmerge/src/main/resources/dbdata/data_m_data_name.sql
@@ -0,0 +1,4 @@
+INSERT INTO m_data_name(id, data_name) VALUES('0001', '商品A');
+INSERT INTO m_data_name(id, data_name) VALUES('0002', '商品B');
+INSERT INTO m_data_name(id, data_name) VALUES('0003', '商品C');
+INSERT INTO m_data_name(id, data_name) VALUES('0004', '商品D');
