8135a6c3ca4dedb425412af6e3da35838866341c
eiiga <ei.tktcwk.ud.f.603@gmail.com>
2024-11-03 12:04:05 +0900

    [java] ExpectedValueCheck: 相互比較（期待値 -> 実測値 ）追加

--- java/expectedvaluecheck/src/checker/InputExpectedChecker.java
+++ java/expectedvaluecheck/src/checker/InputExpectedChecker.java
@@ -1,67 +1,121 @@
 package checker;
 
 import java.util.HashMap;
-
 import constant.CsvHeaderConstant;
 
+// 相互比較クラス
 public class InputExpectedChecker {
 
-	public static void expectedInputChecker(HashMap<Integer, HashMap<String, String>> expectedDatas,
-			HashMap<Integer, HashMap<String, String>> inputDatas) {
+  /*
+   * 期待値 -> 実測値 比較処理
+   * 
+   * @param expectedDatas 期待値HashMap
+   * 
+   * @param inputDatas 予測値HashMap
+   */
+  public static void expectedInputChecker(HashMap<Integer, HashMap<String, String>> expectedDatas,
+      HashMap<Integer, HashMap<String, String>> inputDatas) {
+    // チェックフラグ（true:比較OK、false：比較NG）
+    boolean isData = true;
 
-		boolean isData = true;
+    // csvヘッダ部要素配列
+    final CsvHeaderConstant[] keyHeaders = CsvHeaderConstant.values();
 
-		// csvヘッダ部要素配列
-		final CsvHeaderConstant[] keyHeaders = CsvHeaderConstant.values();
+    System.out.println("##### 相互参照：期待値->実測値 #####");
 
-		if (!expectedDatas.equals(inputDatas)) {
+    // 期待値のキー項目分繰り返し
+    for (Integer keyCount : expectedDatas.keySet()) {
 
-			System.out.println("##### 相互参照：期待値->実測値 #####");
+      // チェックフラグ初期化
+      isData = true;
 
-			for (Integer keyCount : expectedDatas.keySet()) {
+      System.out.println("===== データ：" + keyCount + " =====");
 
-				isData = true;
+      // csvヘッダ部配列要素分繰り返し
+      for (CsvHeaderConstant keyHeader : keyHeaders) {
 
-				System.out.println("===== データ：" + keyCount + " =====");
-				// csvヘッダ部配列要素分繰り返し
-				for (CsvHeaderConstant keyHeader : keyHeaders) {
+        // ヘッダ部が日付の項目はチェック対象外
+        if (CsvHeaderConstant.DATE.equals(keyHeader)) {
+          continue;
+        }
 
-					if (CsvHeaderConstant.DATE.equals(keyHeader)) {
-						continue;
-					}
+        // 実測値にデータが存在しない場合
+        if (inputDatas.get(keyCount) == null) {
 
-					if (inputDatas.get(keyCount) == null) {
-						System.out.println("*** " + keyHeader.getCsvHeaderConstant() + " ***");
-						System.out.println("期待値：" + expectedDatas.get(keyCount).get(keyHeader.getCsvHeaderConstant()));
-						System.out.println("実測値：データなし");
+          // 結果出力
+          System.out.println("*** " + keyHeader.getCsvHeaderConstant() + " ***");
+          System.out
+              .println("期待値：" + expectedDatas.get(keyCount).get(keyHeader.getCsvHeaderConstant()));
+          System.out.println("実測値：データなし");
 
-						isData = false;
+          // チェックフラグNG
+          isData = false;
 
-					} else if (!expectedDatas.get(keyCount).get(keyHeader.getCsvHeaderConstant())
-							.equals(inputDatas.get(keyCount).get(keyHeader.getCsvHeaderConstant()))) {
-						System.out.println("*** " + keyHeader.getCsvHeaderConstant() + " ***");
-						System.out.println("期待値：" + expectedDatas.get(keyCount).get(keyHeader.getCsvHeaderConstant()));
-						System.out.println("実測値：" + inputDatas.get(keyCount).get(keyHeader.getCsvHeaderConstant()));
+          // 期待値と実測値が異なる場合
+        } else if (!expectedDatas.get(keyCount).get(keyHeader.getCsvHeaderConstant())
+            .equals(inputDatas.get(keyCount).get(keyHeader.getCsvHeaderConstant()))) {
 
-						isData = false;
+          // 結果出力
+          System.out.println("*** " + keyHeader.getCsvHeaderConstant() + " ***");
+          System.out
+              .println("期待値：" + expectedDatas.get(keyCount).get(keyHeader.getCsvHeaderConstant()));
+          System.out
+              .println("実測値：" + inputDatas.get(keyCount).get(keyHeader.getCsvHeaderConstant()));
 
-					}
-				}
+          // チェックフラグNG
+          isData = false;
 
-				if (isData) {
-					System.out.println("OK");
-				}
+        }
+      }
+      // チェックフラグOKの場合
+      if (isData) {
+        // 結果出力
+        System.out.println("OK");
+      }
+    }
+  }
 
-			}
-		}
-	}
+  /*
+   * 実測値 -> 期待値 比較処理
+   * 
+   * @param expectedDatas 期待値HashMap
+   * 
+   * @param inputDatas 予測値HashMap
+   */
+  public static void inputExpectedChecker(HashMap<Integer, HashMap<String, String>> expectedDatas,
+      HashMap<Integer, HashMap<String, String>> inputDatas) {
 
-	public static void inputExpectedChecker(HashMap<Integer, HashMap<String, String>> expectedDatas,
-			HashMap<Integer, HashMap<String, String>> inputDatas) {
+    // チェックフラグ（true:比較OK、false：比較NG）
+    boolean isData = true;
 
-		System.out.println("##### 相互参照：実測値->期待値 #####");
+    // csvヘッダ部要素配列
+    final CsvHeaderConstant[] keyHeaders = CsvHeaderConstant.values();
 
-		// TODO: 相互参照：実測値->期待値を実装
-	}
+    System.out.println("##### 相互参照：実測値->期待値 #####");
 
+    for (Integer keyCount : inputDatas.keySet()) {
+      if (expectedDatas.get(keyCount) == null) {
+        System.out.println("===== データ：" + keyCount + " =====");
+        // csvヘッダ部配列要素分繰り返し
+        for (CsvHeaderConstant keyHeader : keyHeaders) {
+          if (CsvHeaderConstant.DATE.equals(keyHeader)) {
+            continue;
+          }
+
+          System.out.println("*** " + keyHeader.getCsvHeaderConstant() + " ***");
+          System.out.println("期待値：データなし");
+          System.out
+              .println("実測値：" + inputDatas.get(keyCount).get(keyHeader.getCsvHeaderConstant()));
+
+          // チェックフラグNG
+          isData = false;
+        }
+      }
+    }
+    // チェックフラグOKの場合
+    if (isData) {
+      // 結果出力
+      System.out.println("OK");
+    }
+  }
 }
