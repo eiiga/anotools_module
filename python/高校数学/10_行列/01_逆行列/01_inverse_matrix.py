05e6392de577fd864b5d89f6ca4634ac0b164a2e
eiiga <ei.tktcwk.ud.f.603@gmail.com>
2024-12-01 12:33:19 +0900

    [python] inverse_matrix: リファクタリング

--- python/高校数学/10_行列/01_逆行列/01_inverse_matrix.py
+++ python/高校数学/10_行列/01_逆行列/01_inverse_matrix.py
@@ -1,5 +1,7 @@
+from fractions import Fraction
 import numpy as np
 
+# 逆行列算出力
 def inverse_matrix_manual(matrix):
     # 行列が正方行列であることを確認
     if matrix.shape[0] != matrix.shape[1]:
@@ -9,28 +11,26 @@ def inverse_matrix_manual(matrix):
     det = matrix[0, 0] * matrix[1, 1] - matrix[0, 1] * matrix[1, 0]
     if det == 0:
         raise ValueError("逆行列は存在しません。")
-    
-    # 余因子行列を計算
-    n = matrix.shape[0]
+       
+    # 逆行列配列：float型で0埋め正方配列を作成
     cofactor_matrix = np.zeros_like(matrix, dtype=float)
-    for i in range(n):
-        for j in range(n):
-            # 小行列を計算
-            minor = np.delete(np.delete(matrix, i, axis=0), j, axis=1)
-            cofactor_matrix[i, j] = ((-1) ** (i + j)) * np.linalg.det(minor)
     
-    # 随伴行列（余因子行列の転置）
-    adjugate = cofactor_matrix.T
-    
+    # 逆行列配列に値を配置
+    cofactor_matrix[0][0] = matrix[1][1]
+    cofactor_matrix[0][1] = -1 * matrix[0][1]
+    cofactor_matrix[1][0] = -1 * matrix[1][0]
+    cofactor_matrix[1][1] = matrix[0][0]
+        
     # 逆行列を計算
-    inverse = adjugate / det
+    inverse = cofactor_matrix / det
+
     return inverse
 
 
-
+# メイン処理
 if __name__ == '__main__':
 
-    # サンプル行列
+    # 問題の行列
     matrix_1 = np.array([[2, 6], [1, 3]])
     matrix_2 = np.array([[-3, 4], [2, -3]])
     matrix_3 = np.array([[5, 2], [1, 0]])
@@ -48,8 +48,10 @@ for matrix in matrix_list:
     try:
         result = inverse_matrix_manual(matrix)
         print("元の行列:")
-        print(matrix)
+        # 少数の場合は分数表記
+        print(np.vectorize(lambda x: str(Fraction(x).limit_denominator()))(matrix))
         print("\n逆行列:")
-        print(result)
+        # 少数の場合は分数表記
+        print(np.vectorize(lambda x: str(Fraction(x).limit_denominator()))(result))
     except ValueError as e:
         print(e)
